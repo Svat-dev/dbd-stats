3d296c445c0fb10c822bf9bfb19191a8d4ce2800
Svat-dev <svatoslavgetun@gmail.com>
2026-07-01 04:34:35 +0300

    feat: serve static route done

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import "dotenv/config"
 
 import express from "express"
+import * as path from "node:path"
+import { fileURLToPath } from "node:url"
 import { StatsRoute } from "./routes/stats.js"
 import { Logger } from "./shared/logger.js"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 
 app.disable("x-powered-by")
 
 app.use(Logger)
+
+app.use(express.static(path.join(__dirname, "..", "public")))
 
 app.get("/dbd/stats", StatsRoute)
 
